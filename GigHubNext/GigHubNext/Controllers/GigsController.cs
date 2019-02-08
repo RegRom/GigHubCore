75db4fd0866099f6cc5422e18b3480016dfbb5c7
Romeo Rego <Romeo.Rego@unit4.com>
2019-02-08 16:55:13 +0100

    Added Gigs Controller, in progress of making View for creating Gigs

---
+++ GigHubNext/GigHubNext/Controllers/GigsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GigHubNext.Controllers
+{
+    public class GigsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

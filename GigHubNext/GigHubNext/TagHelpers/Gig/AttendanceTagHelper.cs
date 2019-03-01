3b3f2ceb3e19691527f8a836ce0288af21880ccf
Romeo Rego <Romeo.Rego@unit4.com>
2019-03-01 16:44:38 +0100

    Some fixes to "Attend" button (UNFINISHED)

---
+++ GigHubNext/GigHubNext/TagHelpers/Gig/AttendanceTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GigHubNext.TagHelpers.Gig
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("attendance")]
+    public class AttendanceTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            output.Content.SetHtmlContent("Going?");
+
+            output.Attributes.Add("class", "btn btn-default btn-sm pull-right");
+
+            
+        }
+    }
+}
